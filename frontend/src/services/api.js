1d21b2f16f8fde9474741a8bdc2aa1c1803f1f86
Prashant Kumar <125740796+Fred-kumar@users.noreply.github.com>
2026-05-19 21:58:45 +0530

    Update API_URL to point to production server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useAuthStore } from '../context/store';
 
-const API_URL = process.env.REACT_APP_API_URL || '/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://nexus-vsbb.onrender.com//api';
 
 const api = axios.create({
   baseURL: API_URL,
